64b44e3a0742d278690ff8d4b7430630fe101c8d
sihamfich <sihamfichouche@gmail.com>
2024-01-08 12:53:19 -0800

    Task 0 lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function"""
+
+
+def lookup(obj):
+    """
+    looks up object attributes and methods
+    Args:
+        obj (object): the object to list
+    Return:
+        a list of an object's available attributes"""
+    return (dir(obj))
