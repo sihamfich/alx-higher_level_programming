f71e2fddb2e49f0de85a43eae0e98430b62a808b
SIHAM <sihamfichouche@gmail.com>
2024-07-30 14:12:31 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for element in str:
         if ord(element) >= 97 and ord(element) <= 122:
-            i = chr(ord(element) - 32)
+            element = chr(ord(element) - 32)
         print("{}".format(element), end="")
     print()
